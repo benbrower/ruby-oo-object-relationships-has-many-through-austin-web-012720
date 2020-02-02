b328c2f19279351f54aaffec23892c996145b9a9
mystical-calculator-5374 <bbrower@smu.edu>
2020-02-02 21:50:47 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
+  attr_accessor :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, tip)
+    @waiter = waiter
+    @customer = customer
+    @tip = tip
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
